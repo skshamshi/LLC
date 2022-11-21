82341750891d7d688a26a23c0b025bd596b5a962
prapalli <117046279+prapalli@users.noreply.github.com>
2022-11-21 03:40:43 -0800

    mapping intial draft

---
+++ .github/mapping.py
@@ -0,0 +1,12 @@
+def address_mapping (address):
+  import math
+  address = address
+# Code to convert hex to binary
+  result = "{0:032b}".format(int(address, 16))
+  tag_bits = str(result[0:11])
+  index = str(result[11:26])
+  byte_select_bits = str (result[26:32])
+  return tag_bits,index,byte_select_bits
+     
+Tag,Index,Byteselect = address_mapping ("1ABCDEF0")
+print (Tag)
