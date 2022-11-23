4faf6a8a0751d14d468b7f4b388b8e6b3b1fd982
nithinfor9 <nithin4@pdx.edu>
2022-11-22 20:46:30 -0800

    Update cache.py

--- .github/LRU/cache.py
+++ .github/LRU/cache.py
@@ -1,3 +1,11 @@
-m, n = (5, 5)
-arr = [[1]*n]*m
+import pprint
+col, row = (5, 5)
+arr = [[1]*col]*row
 print(arr)
+
+associativity=8
+total_lines=6
+cache =[['x' for i in range (associativity)] for k in range(total_lines)]
+#pprint.pprint(cache)
+cache[3][6] = 0x000032AC
+pprint.pprint(cache)
