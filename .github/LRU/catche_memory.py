85b2fc4d49c8f34fb7c6e60a479e209f063d7550
prapalli <117046279+prapalli@users.noreply.github.com>
2022-11-26 11:22:52 -0800

    update in catche code by praveen
    added loops successfully

--- .github/LRU/catche_memory.py
+++ .github/LRU/catche_memory.py
@@ -1,3 +1,6 @@
+Sets = 32768
+Associative = 8
+cache = []
 class line:
    def __init__ (self,MESI,TAG):
       self.MESI = MESI
@@ -6,9 +9,17 @@ class set:
    def __init__(self,LRU,LINE):
       self.LRU = LRU
       self.LINE = LINE
-z= line ([],[])
+#z= line ([],[])
 #print (z.MESI)
-x = set ("lru",z)
-x.LINE.MESI = "HELLO"
+#x = set ("lru",z)
+#x.LINE.MESI = "HELLO"
 
-print (x.LINE.MESI)
+for i in range (1,Sets):
+   i = set ([],[])
+   for j in range (1,Associative):
+      j = line ([],[])
+      i.LINE.append(j)
+cache.append(i)
+
+#cache =[[ for i in range (associativity)] for k in range(total_lines)]
+print (cache[Sets[0]])
