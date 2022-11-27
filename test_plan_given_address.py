f967c832b571186ac8725c2bcd4df1ad0c57f40e
shivaprasadbalne <sbalne@pdx.edu>
2022-11-27 12:35:13 -0800

    Update test_plan_given_address.py

--- test_plan_given_address.py
+++ test_plan_given_address.py
@@ -1,4 +1,4 @@
-if(address>0x11111111 & address<FFFFFFFF)
+if(address>0x00000001 & address<0xFFFFFFFF)
 print('error, given address is invalid')
 else if(address= 0x00000000)
 print('error, given address is invalid')
