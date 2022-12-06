ae5378854687a1ec3a290c417fe37c4ef2afa275
prapalli <117046279+prapalli@users.noreply.github.com>
2022-12-05 16:49:10 -0800

    updated LRU in main function
    updated LRU in main function

--- .github/LRU/catche_memory.py
+++ .github/LRU/catche_memory.py
@@ -1,16 +1,3 @@
-#import os
-#filename1 = "c:\Users\student\Desktop\Project final\LLC\.github\LRU\catche_memory.py"
-#filename = input("enter path")
-#print (filename)
-
-#(filename)
-#print (filename)
-
-#mapping of address 
-#commit comment1
-#
-# 
-# hello
 def address_mapping (address):
   import math
   address = address
@@ -60,54 +47,123 @@ class set:
 #intialsing empty cache arrays
 for i in range (1,Sets):
    i = set ([],[])
+   i.LRU = 0
    for j in range (0,Associative):
       j = line ([],[])
-      j.MESI = "0"
+      j.MESI = 0
       i.LINES.append (j)
    cache.append(i)
 
 
 def Hit_or_Miss (Index_decimal,Tag_decimal):
+   line_count =  0
+   empty_lines = 0
    if (Index_decimal > Sets):
       print ("out of cache bound")
    else:
       selected_set = cache [Index_decimal]
-      lines_count = 0
-      Invalid_lines_count = 0
       for line_obj in (selected_set.LINES):
-            lines_count += 1
-            if(line_obj.MESI == "0"):
-               Invalid_lines_count += 1
-               if (line_obj.TAG != Tag_decimal):
+            if(line_obj.MESI == 0):
+               empty_lines += 1
+               line_count += 1
+               if (line_count != 8):
                   continue
-               line_obj.TAG = Tag_decimal
-               line_obj.MESI = "1"
-               break
-            elif(line_obj.MESI == "1" and line_obj.TAG ==Tag_decimal):
-                  print ("HIT")
-                  break
-            if (lines_count == 8):
-               print("calling LRU")
+               else:
+                  return 'MISS',empty_lines,line_count
+            else:
+               if(line_obj.TAG !=Tag_decimal ):
+                  line_count += 1
+                  if (line_count != 8):
+                     continue
+                  else:
+                     return 'MISS',empty_lines,line_count
+               else:
+                  line_count += 1
+                  return 'HIT',empty_lines,line_count
+
+def catch_fill (Index_decimal,Tag_decimal):
+   selected_set = cache [Index_decimal]
+   for line_obj in (selected_set.LINES):
+      if(line_obj.MESI == 1):
+         continue
+      else:
+       line_obj.TAG = Tag_decimal
+       line_obj.MESI = 1
+       break
+def update_LRU (set,way):
+    selected_set = cache [Index_decimal]
+    print (set)
+    way = "{0:03b}".format(way)
+    print (way)
+    LRU_list = [str(i) for i in list('{0:07b}'.format(selected_set.LRU))]
+    #int(i) for i in list('{0:0b}'.format(test_num))]
+    if (way[0] == "0"):
+     LRU_list[0] = "0"
+     if (way[1] == "0"):
+        LRU_list[1] = "0"
+        if (way[2] == "0"):
+            LRU_list[3] = "0"
+        else:
+            LRU_list[3] = "1"
+     else:
+        LRU_list[1] = "1"
+        if (way[2] == "0"):
+            LRU_list[4] = "0"
+        else:
+            LRU_list[4] = "1"
+
+    else:
+     LRU_list[0] = "1"
+     if (way[1] == "0"):
+        LRU_list[2] = "0"
+        if (way[2] == "0"):
+            LRU_list[5] = "0"
+        else:
+            LRU_list[5] = "1"
+     else:
+        LRU_list[2] = "1"
+        if (way[2] == "0"):
+            LRU_list[6] = "0"
+        else:
+            LRU_list[6] = "1"
+    print (LRU_list)
+    print(''.join(LRU_list))
 
 
-#cache [Index_decimal]
-Hit_or_Miss(Index_decimal,Tag_decimal)
-Hit_or_Miss(Index_decimal,Tag_decimal)
-Hit_or_Miss(Index_decimal,Tag_decimal2)
-Hit_or_Miss(Index_decimal,Tag_decimal2)
-Hit_or_Miss(Index_decimal,Tag_decimal3)
-Hit_or_Miss(Index_decimal,Tag_decimal3)
-Hit_or_Miss(Index_decimal,Tag_decimal4)
-Hit_or_Miss(Index_decimal,Tag_decimal4)
-Hit_or_Miss(Index_decimal,Tag_decimal5)
-Hit_or_Miss(Index_decimal,Tag_decimal5)
-Hit_or_Miss(Index_decimal,Tag_decimal6)
-Hit_or_Miss(Index_decimal,Tag_decimal6)
-Hit_or_Miss(Index_decimal,Tag_decimal7)
-Hit_or_Miss(Index_decimal,Tag_decimal7)
-Hit_or_Miss(Index_decimal,Tag_decimal8)
-Hit_or_Miss(Index_decimal,Tag_decimal8)
-Hit_or_Miss(Index_decimal,Tag_decimal9)
-Hit_or_Miss(Index_decimal,Tag_decimal9)
+def catch_search (Index_decimal,Tag_decimal):
+   y,empty_lines,line_count = Hit_or_Miss(Index_decimal,Tag_decimal)
+   if (y == "HIT"):
+      #print ("HIT")
+      #print ("valid data is in way",line_count-1)
+      update_LRU (Index_decimal,line_count-1)
+      #way_binary = ("{0:03b}".format(line_count-1))
+      #print (way_binary[0])
+      #print ("calling update LRU function")
+   elif (y == "MISS" and empty_lines != 0):
+      #print (empty_lines)
+      #print ("MISS")
+      catch_fill (Index_decimal,Tag_decimal)
+   else:
+    print ("calling LRU")
 
-#print(Tag_decimal)
+
+catch_search(Index_decimal,Tag_decimal2)
+catch_search(Index_decimal,Tag_decimal2)
+
+catch_search(Index_decimal,Tag_decimal)
+catch_search(Index_decimal,Tag_decimal)
+
+catch_search(Index_decimal,Tag_decimal3)
+catch_search(Index_decimal,Tag_decimal3)
+catch_search(Index_decimal,Tag_decimal4)
+catch_search(Index_decimal,Tag_decimal4)
+catch_search(Index_decimal,Tag_decimal5)
+catch_search(Index_decimal,Tag_decimal5)
+catch_search(Index_decimal,Tag_decimal6)
+catch_search(Index_decimal,Tag_decimal6)
+catch_search(Index_decimal,Tag_decimal7)
+catch_search(Index_decimal,Tag_decimal7)
+catch_search(Index_decimal,Tag_decimal8)
+catch_search(Index_decimal,Tag_decimal8)
+catch_search(Index_decimal,Tag_decimal9)
+catch_search(Index_decimal,Tag_decimal9)
